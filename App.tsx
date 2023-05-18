37d86543ca20fe7dd40d8c4a8a473d0930ec3f81
Mikael Grön <skaramicke@gmail.com>
2023-05-18 16:25:30 +0200

    Add nativebase example signin form

--- App.tsx
+++ App.tsx
@@ -1,118 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  Heading,
+  Input,
+  NativeBaseProvider,
+  VStack,
+} from 'native-base';
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
+const SignupForm = () => (
+  <Center w="100%">
+    <Box safeArea p="2" w="90%" maxW="290" py="8">
+      <Heading
+        size="lg"
+        color="coolGray.800"
+        _dark={{
+          color: 'warmGray.50',
+        }}
+        fontWeight="semibold">
+        Welcome
+      </Heading>
+      <Heading
+        mt="1"
+        color="coolGray.600"
+        _dark={{
+          color: 'warmGray.200',
+        }}
+        fontWeight="medium"
+        size="xs">
+        Sign up to continue!
+      </Heading>
+      <VStack space={3} mt="5">
+        <FormControl>
+          <FormControl.Label>Email</FormControl.Label>
+          <Input />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>Password</FormControl.Label>
+          <Input type="password" />
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>Confirm Password</FormControl.Label>
+          <Input type="password" />
+        </FormControl>
+        <Button mt="2" colorScheme="indigo">
+          Sign up
+        </Button>
+      </VStack>
+    </Box>
+  </Center>
+);
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <SignupForm />
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
