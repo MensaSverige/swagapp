2c31fc2404bd64a8d92007745a2e376fd2a62a74
Mikael Grön <skaramicke@gmail.com>
2023-05-18 19:46:56 +0200

    Remove jsdom dependency from auth function

--- functions/auth.ts
+++ functions/auth.ts
@@ -1,5 +1,3 @@
-import JSDOM from 'jsdom';
-
 interface Env {
   KV: KVNamespace;
 }
@@ -18,24 +16,18 @@ export const onRequest: PagesFunction<Env> = async context => {
   // Make a GET request to the login page and parse the HTML.
   const loginPageResponse = await fetch(loginPageURL);
   const loginPageHTML = await loginPageResponse.text();
-  const dom = new JSDOM(loginPageHTML);
-  const document = dom.window.document;
 
-  // Find the CSRF token and other hidden form values.
-  const csrfToken = document.querySelector('input[name="csrfKey"]').value;
-  const ref = document.querySelector('input[name="ref"]').value;
-  const MAX_FILE_SIZE = document.querySelector(
-    'input[name="MAX_FILE_SIZE"]',
-  ).value;
-  const plupload = document.querySelector('input[name="plupload"]').value;
+  // Find the CSRF token from the HTML.
+  const csrfTokenStart =
+    loginPageHTML.indexOf('name="csrfKey" value="') +
+    'name="csrfKey" value="'.length;
+  const csrfTokenEnd = loginPageHTML.indexOf('"', csrfTokenStart);
+  const csrfToken = loginPageHTML.substring(csrfTokenStart, csrfTokenEnd);
 
   // Create the form data for the login request.
   const loginFormData = new URLSearchParams();
   loginFormData.append('login__standard_submitted', '1');
   loginFormData.append('csrfKey', csrfToken);
-  loginFormData.append('ref', ref);
-  loginFormData.append('MAX_FILE_SIZE', MAX_FILE_SIZE);
-  loginFormData.append('plupload', plupload);
   loginFormData.append('auth', requestBody.username);
   loginFormData.append('password', requestBody.password);
   loginFormData.append('remember_me', '0');
@@ -58,9 +50,14 @@ export const onRequest: PagesFunction<Env> = async context => {
 
   // Otherwise, parse the returned HTML for the user's name.
   const userPageHTML = await loginResponse.text();
-  const userDom = new JSDOM(userPageHTML);
-  const userDocument = userDom.window.document;
-  const userName = userDocument.querySelector('#elUserLink').textContent.trim();
+
+  // Find the user's name from the HTML.
+  const userNameStart =
+    userPageHTML.indexOf(
+      '<a href="#elUserLink_menu" id="elUserLink" data-ipsmenu>',
+    ) + '<a href="#elUserLink_menu" id="elUserLink" data-ipsmenu>'.length;
+  const userNameEnd = userPageHTML.indexOf('<', userNameStart);
+  const userName = userPageHTML.substring(userNameStart, userNameEnd).trim();
 
   return new Response(`Hello, ${userName}!`, {
     headers: {'Content-Type': 'text/plain'},
