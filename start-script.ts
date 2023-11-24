cd461ae8f5dfcd0897eacbcc129e7fb237d08abb
Mikael Grön <skaramicke@users.noreply.github.com>
2023-11-24 22:03:29 +0100

    Resolve to localhost on error (#45)

--- start-script.ts
+++ start-script.ts
@@ -1,19 +1,20 @@
-import { exec } from 'child_process';
+import {exec} from 'child_process';
 import fs from 'fs';
 
-
 const getWifiIPAddress = (): Promise<string> => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, _) => {
     exec('netsh interface ip show address "Wi-Fi"', (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
-        return reject(error);
+        return resolve('localhost');
       }
       if (stderr) {
         console.error(`Error: ${stderr}`);
-        return reject(new Error(stderr));
+        return resolve('localhost');
       }
-      const ipLine = stdout.split('\n').find(line => line.trim().startsWith('IP Address'));
+      const ipLine = stdout
+        .split('\n')
+        .find(line => line.trim().startsWith('IP Address'));
       if (ipLine) {
         const ipAddress = ipLine.split(':')[1].trim();
         console.log(`Wi-Fi IP Address: ${ipAddress}`);
@@ -29,8 +30,10 @@ const getWifiIPAddress = (): Promise<string> => {
 const setEnvironmentVariables = async (): Promise<void> => {
   try {
     const isProd = process.argv.includes('--prod');
-    const backendURL = isProd ? 'https://swag.mikael.green/api' : `http://${await getWifiIPAddress()}:5000`;
-    
+    const backendURL = isProd
+      ? 'https://swag.mikael.green/api'
+      : `http://${await getWifiIPAddress()}:5000`;
+
     fs.writeFileSync('.env', `API_URL=${backendURL}\nTEST_MODE=${!isProd}\n`);
 
     console.log(`Using Backend URL: ${backendURL}`);
