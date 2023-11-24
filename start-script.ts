75f8fe823f4b34df9db9e7113db87289c2e68483
Imamyable <37723862+Imamyable@users.noreply.github.com>
2023-11-24 20:07:29 +0100

    local be script, set and use environment variables (#43)
    * local backend script, set and use environment variables

---
+++ start-script.ts
@@ -0,0 +1,43 @@
+import { exec } from 'child_process';
+import fs from 'fs';
+
+
+const getWifiIPAddress = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    exec('netsh interface ip show address "Wi-Fi"', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return reject(error);
+      }
+      if (stderr) {
+        console.error(`Error: ${stderr}`);
+        return reject(new Error(stderr));
+      }
+      const ipLine = stdout.split('\n').find(line => line.trim().startsWith('IP Address'));
+      if (ipLine) {
+        const ipAddress = ipLine.split(':')[1].trim();
+        console.log(`Wi-Fi IP Address: ${ipAddress}`);
+        resolve(ipAddress);
+      } else {
+        console.log('Wi-Fi IP Address not found.');
+        resolve('localhost');
+      }
+    });
+  });
+};
+
+const setEnvironmentVariables = async (): Promise<void> => {
+  try {
+    const isProd = process.argv.includes('--prod');
+    const backendURL = isProd ? 'https://swag.mikael.green/api' : `http://${await getWifiIPAddress()}:5000`;
+    
+    fs.writeFileSync('.env', `API_URL=${backendURL}\nTEST_MODE=${!isProd}\n`);
+
+    console.log(`Using Backend URL: ${backendURL}`);
+    console.log(`Test mode is set to: ${!isProd}`);
+  } catch (error: any) {
+    console.error(`Failed to set backend URL: ${error.message}`);
+  }
+};
+
+setEnvironmentVariables();
