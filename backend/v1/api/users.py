f8aaf2a9ac59b9b2607144844e400c3f44d55b03
Mikael Grön <skaramicke@gmail.com>
2024-04-14 15:13:47 +0200

    Remove trailing slash on /users/me endpoint

--- backend/v1/api/users.py
+++ backend/v1/api/users.py
@@ -5,16 +5,18 @@ from db.users import get_user, get_users
 
 users_v1 = APIRouter(prefix="/v1")
 
+
 @users_v1.get("/users/{user_id}")
 async def get_user_by_id(user_id: int):
     user = get_user(user_id)
     return user
-    
+
 
 @users_v1.get("/users_showing_location")
 def users_showing_location(current_user: User = Depends(validate_request)):
     return get_users(show_location=True)
 
-@users_v1.get("/users/me/", response_model=User)
+
+@users_v1.get("/users/me", response_model=User)
 async def get_current_user(current_user: User = Depends(validate_request)):
     return current_user
