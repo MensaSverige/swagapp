084f13119b93e280ca6a61282afa6e0ce44b7131
Mikael Grön <skaramicke@gmail.com>
2024-04-30 17:02:51 +0200

    Make news view full scroll view

--- app/features/common/screens/WelcomeScreen.tsx
+++ app/features/common/screens/WelcomeScreen.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import SiteNews from './SiteNews';
-import { Divider, Heading, VStack } from '../../../gluestack-components';
+import { Divider, Heading, ScrollView, VStack } from '../../../gluestack-components';
 import StartscreenFaq from './StartscreenFaq';
 
 const WelcomeScreen = () => {
     return (
 
         <VStack space="lg" h="100%" bg="$background0" flex={1} paddingHorizontal={20}>
-            <Heading size="xl" >Välkommen till SWAG!</Heading>
-            <StartscreenFaq />
-            <Divider/>
-            <SiteNews />
+            <ScrollView>
+                <VStack space={"lg"}>
+                    <Heading size="xl" >Välkommen till SWAG!</Heading>
+                    <StartscreenFaq />
+                    <Divider/>
+                    <SiteNews />
+                </VStack>
+            </ScrollView>
         </VStack>
 
     );
