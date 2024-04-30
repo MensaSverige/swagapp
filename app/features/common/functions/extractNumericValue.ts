1dad6644ba308c5f907f54c96b019f219af5eb38
Imamyable <37723862+Imamyable@users.noreply.github.com>
2024-04-30 20:54:10 +0200

    Feature/fix field bugs (#90)
    * fix phonenumber starts w 0
    * default height 48

--- app/features/common/functions/extractNumericValue.ts
+++ app/features/common/functions/extractNumericValue.ts
@@ -1,4 +1,4 @@
 export const extractNumericValue = (input: string) => {
     const numericValue = input.replace(/[^0-9]/g, '');
-    return numericValue ? parseInt(numericValue) : null;
+    return numericValue ? numericValue : null;
 }
