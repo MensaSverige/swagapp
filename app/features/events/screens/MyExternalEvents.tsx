caeac08fcf31ef8596624f3a5d961c0f85a396c0
Mikael Grön <skaramicke@gmail.com>
2024-05-06 21:01:20 +0200

    Add spontaneous events to schema view

--- app/features/events/screens/MyExternalEvents.tsx
+++ app/features/events/screens/MyExternalEvents.tsx
@@ -24,6 +24,8 @@ import {
 } from '../components/EventBadges';
 import ExternalEventCardModal from '../components/ExternalEventCardModal';
 import { config } from '../../../gluestack-components/gluestack-ui.config';
+import { useEventLists } from '../hooks/useEventLists';
+import FutureUserEvent from '../types/futureUserEvent';
 
 
 export const displayLocaleTimeStringDate = (datestring: string) => {
@@ -58,36 +60,75 @@ const getEventCategoryBadge = (categoryCode: string, color: string) => {
     }
 }
 
+type EventItem = ExternalEventDetails | FutureUserEvent;
+
 export const MyExternalEvents = () => {
     const vscode_customLiteral = useToken("colors", "vscode_customLiteral")
     const vscode_numberLiteral = useToken("colors", "vscode_numberLiteral")
     const [events, setEvents] = useState<ExternalEventDetails[]>();
-    const [groupedEvents, setGroupedEvents] = useState<{ [key: string]: ExternalEventDetails[] }>({});
-    const [selectedEvent, setSelectedEvent] = useState<ExternalEventDetails | null>(events ? events[0] : null);
+    const [groupedEvents, setGroupedEvents] = useState<{ [key: string]: (EventItem)[] }>({});
+    const [selectedEvent, setSelectedEvent] = useState<EventItem | null>(events ? events[0] : null);
     const [loading, setLoading] = useState(false);
     const { user } = useStore();
 
+
+    const {
+        visibleEvents,
+        eventsRefreshing,
+        fetchAllEvents,
+        subscribe,
+        unsubscribe,
+    } = useEventLists();
+
+    React.useEffect(() => {
+        subscribe('schedule view');
+        return () => {
+        unsubscribe('schedule view');
+        };
+    }, [subscribe, unsubscribe]);
+
+    useEffect(() => {
+        const myUserEvents = user
+            ? (visibleEvents as FutureUserEvent[]).filter(
+                  e =>
+                      e.attendees?.includes({ userId: user.userId }) ||
+                      e.hosts?.includes({ userId: user.userId }) ||
+                      e.userId === user.userId,
+              )
+            : [];
+        const allEvents: EventItem[] = [...myUserEvents, ...(events ?? [])];
+        const newGroupedEvents: { [key: string]: EventItem[] } =
+            allEvents.reduce((grouped, event) => {
+                let date: string = '';
+                if ('eventDate' in event && event.eventDate) {
+                    date = new Date(event.eventDate).toDateString();
+                } else if ('start' in event && event.start) {
+                    date = new Date(event.start).toDateString();
+                }
+                if (!grouped[date]) {
+                    grouped[date] = [];
+                }
+                grouped[date].push(event);
+                return grouped;
+            }, {} as { [key: string]: EventItem[] });
+        setGroupedEvents(newGroupedEvents);
+    }, [user, events, visibleEvents]);
+
     const onRefresh = useCallback(() => {
         setLoading(true);
+        
         fetchExternalEvents().then((events) => {
             events.sort((a, b) => {
                 const dateA = a.eventDate ? new Date(a.eventDate).getTime() : 0;
                 const dateB = b.eventDate ? new Date(b.eventDate).getTime() : 0;
                 return dateA - dateB;
             });
-
-            const newGroupedEvents: { [key: string]: ExternalEventDetails[] } = events.reduce((grouped, event) => {
-                const date = event.eventDate ? new Date(event.eventDate).toDateString() : 'No Date';
-                if (!grouped[date]) {
-                    grouped[date] = [];
-                }
-                grouped[date].push(event);
-                return grouped;
-            }, {} as { [key: string]: ExternalEventDetails[] });
-
-            setGroupedEvents(newGroupedEvents);
+            setEvents(events);
             setLoading(false);
         });
+
+        fetchAllEvents();
+        
         console.log('fetching events');
     }, []);
 
@@ -95,14 +136,14 @@ export const MyExternalEvents = () => {
         onRefresh();
     }, [onRefresh]);
 
-    const handlePress = useCallback((event: ExternalEventDetails) => {
+    const handlePress = useCallback((event: EventItem) => {
         setSelectedEvent(event);
     }, []);
 
     return (
 
         <VStack flex={1} space="sm" h="100%" bg="$background0">
-            {selectedEvent && (
+            {selectedEvent && 'eventDate' in selectedEvent && (
                 <ExternalEventCardModal
                     event={selectedEvent}
                     open={!!selectedEvent}
@@ -114,7 +155,7 @@ export const MyExternalEvents = () => {
             <ScrollView flex={1}
                 refreshControl={
                     <RefreshControl
-                        refreshing={loading}
+                        refreshing={loading || eventsRefreshing}
                         onRefresh={onRefresh}
                         tintColor={config.tokens.colors.secondary300}
                         colors={[config.tokens.colors.secondary300]}
@@ -132,37 +173,67 @@ export const MyExternalEvents = () => {
                     <VStack key={date} space="sm" paddingHorizontal={20}>
                         <Heading color="$primary900" size="lg" >{displayLocaleTimeStringDate(date ?? "")}</Heading>
                         <Divider />
-                        {groupedEvents[date].map((event) => (
-                            <Pressable key={event.eventId} onPress={() => handlePress(event)}>
+                        {groupedEvents[date].map((event) => {
 
-                                <HStack key={event.eventId} space="sm" paddingVertical={10}>
-                                    <VStack justifyContent="flex-start" alignItems="center">
-                                        <Text size="md" color='$teal500' paddingTop={2}>
-                                            {event.startTime}
-                                        </Text>
-                                        <Text size='md' color='$teal800'>
-                                            {event.endTime}
-                                        </Text>
-                                    </VStack>
-                                    <VStack flex={1} paddingLeft={10}>
-                                        <HStack space="md" justifyContent="space-between" alignItems="center">
-                                            <Heading size="md" color="$primary600" style={{ flex: 1 }}>
-                                                {event.titel}
-                                            </Heading>
-                                            {event.categories?.map((category, index) => (
-                                                <Text key={index} color="$vscode_customLiteral" style={{ paddingLeft: 10, maxWidth: 45 }}>
-                                                    {getEventCategoryBadge(category.code, `#${category.colorBackground}`)}
-                                                </Text>
-                                            ))}
-                                        </HStack>
-                                        <Text color="$vscode_customLiteral" style={{ marginBottom: 10 }}>
-                                            {/* <FontAwesomeIcon icon={faMountainCity} size={14} style={{ color: vscode_customLiteral, marginRight: 10 }} /> */}
-                                            {event.location}
-                                        </Text>
-                                    </VStack>
-                                </HStack>
-                            </Pressable>
-                        ))}
+                            if ('eventDate' in event) return (
+                                <Pressable key={event.eventId} onPress={() => handlePress(event)}>
+                                    <HStack key={event.eventId} space="sm" paddingVertical={10}>
+                                        <VStack justifyContent="flex-start" alignItems="center">
+                                            <Text size="md" color='$teal500' paddingTop={2}>
+                                                {event.startTime}
+                                            </Text>
+                                            <Text size='md' color='$teal800'>
+                                                {event.endTime}
+                                            </Text>
+                                        </VStack>
+                                        <VStack flex={1} paddingLeft={10}>
+                                            <HStack space="md" justifyContent="space-between" alignItems="center">
+                                                <Heading size="md" color="$primary600" style={{ flex: 1 }}>
+                                                    {event.titel}
+                                                </Heading>
+                                                {event.categories?.map((category, index) => (
+                                                    <Text key={index} color="$vscode_customLiteral" style={{ paddingLeft: 10, maxWidth: 45 }}>
+                                                        {getEventCategoryBadge(category.code, `#${category.colorBackground}`)}
+                                                    </Text>
+                                                ))}
+                                            </HStack>
+                                            <Text color="$vscode_customLiteral" style={{ marginBottom: 10 }}>
+                                                {/* <FontAwesomeIcon icon={faMountainCity} size={14} style={{ color: vscode_customLiteral, marginRight: 10 }} /> */}
+                                                {event.location}
+                                            </Text>
+                                        </VStack>
+                                    </HStack>
+                                </Pressable>
+                            );
+
+                            if ('start' in event) return (
+                                <Pressable key={event.id} onPress={() => handlePress(event)}>
+                                    <HStack key={event.id} space="sm" paddingVertical={10}>
+                                        <VStack justifyContent="flex-start" alignItems="center">
+                                            <Text size="md" color='$teal500' paddingTop={2}>
+                                                {new Date(event.start).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                                            </Text>
+                                            {event.end && (
+                                            <Text size='md' color='$teal800'>
+                                                {new Date(event.end).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+                                            </Text>
+                                            )}
+                                        </VStack>
+                                        <VStack flex={1} paddingLeft={10}>
+                                            <HStack space="md" justifyContent="space-between" alignItems="center">
+                                                <Heading size="md" color="$primary600" style={{ flex: 1 }}>
+                                                    {event.name}
+                                                </Heading>
+                                            </HStack>
+                                            <Text color="$vscode_customLiteral" style={{ marginBottom: 10 }}>
+                                                {/* <FontAwesomeIcon icon={faMountainCity} size={14} style={{ color: vscode_customLiteral, marginRight: 10 }} /> */}
+                                                {event.location?.description}
+                                            </Text>
+                                        </VStack>
+                                    </HStack>
+                                </Pressable>
+                            );
+                        })}
 
                     </VStack>
 
