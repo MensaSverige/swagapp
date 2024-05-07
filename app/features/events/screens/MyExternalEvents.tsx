ce734e6e35111c9ffde661efde9db21a99a729d6
Mikael Grön <skaramicke@users.noreply.github.com>
2024-05-07 08:49:33 +0200

    Feature/scroll to now in my events (#95)
    * Add "go to now" button
    ---------
    
    Co-authored-by: Imamyable <amynylander@hotmail.com>

--- app/features/events/screens/MyExternalEvents.tsx
+++ app/features/events/screens/MyExternalEvents.tsx
@@ -1,4 +1,6 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { NativeScrollEvent, NativeSyntheticEvent, ScrollView as ReactScrollView } from 'react-native';
+import { View } from 'react-native';
 import { fetchExternalEvents } from '../services/eventService';
 import { ExternalEventDetails } from '../../../api_schema/types';
 import {
@@ -7,6 +9,7 @@ import {
     Text,
     Pressable,
     VStack,
+    Icon,
 } from '../../../gluestack-components';
 import { useToken, } from "@gluestack-ui/themed"
 import { LoadingScreen } from '../../common/screens/LoadingScreen';
@@ -23,7 +26,10 @@ import {
     WorkshopBadge
 } from '../components/EventBadges';
 import ExternalEventCardModal from '../components/ExternalEventCardModal';
-
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faHistory } from '@fortawesome/free-solid-svg-icons';
+import { config } from "../../../gluestack-components/gluestack-ui.config";
+import { AlarmClockCheckIcon, CalendarClockIcon, GalleryVerticalEndIcon, HourglassIcon, WatchIcon } from 'lucide-react-native';
 
 export const displayLocaleTimeStringDate = (datestring: string) => {
     const date: Date = new Date(datestring ?? "");
@@ -63,8 +69,13 @@ export const MyExternalEvents = () => {
     const [events, setEvents] = useState<ExternalEventDetails[]>();
     const [groupedEvents, setGroupedEvents] = useState<{ [key: string]: ExternalEventDetails[] }>({});
     const [selectedEvent, setSelectedEvent] = useState<ExternalEventDetails | null>(events ? events[0] : null);
+    const [nextEvent, setNextEvent] = useState<ExternalEventDetails | null>(events ? events[0] : null);
+    const [didInitiallyScroll, setDidInitiallyScroll] = useState(false);
     const [loading, setLoading] = useState(true);
     const { user } = useStore();
+
+    const scrollViewRef = useRef<ReactScrollView>(null);
+    const nextEventMarkerRef = useRef<View>(null);
 
     useEffect(() => {
         fetchExternalEvents().then((events) => {
@@ -89,9 +100,62 @@ export const MyExternalEvents = () => {
         console.log('fetching events');
     }, []);
 
+    useEffect(() => {
+        if (!groupedEvents || Object.keys(groupedEvents).length === 0) return;
+        const findNextEvent = () => {
+            const now = new Date();
+            // const now = new Date(2024, 4, 9, 18, 10, 0);
+            let nextEventTemp: ExternalEventDetails | null = null;
+    
+            for (const date in groupedEvents) {
+                for (const event of groupedEvents[date]) {
+                    if (!event.eventDate) continue;
+                    
+                    const eventDate = new Date(event.eventDate);
+                    eventDate.setHours(parseInt(event.endTime.split(':')[0]), parseInt(event.endTime.split(':')[1]));
+                    if (eventDate > now) {
+                        nextEventTemp = event;
+                        break;
+                    }
+                }
+                if (nextEventTemp) break;
+            }
+            if (nextEventTemp?.eventId === groupedEvents[Object.keys(groupedEvents)[0]][0].eventId) {
+                // Don't show the scroll to next event button if the next event is the first event of the first day
+                nextEventTemp = null;
+            }
+            setNextEvent(nextEventTemp);
+        };
+        const intervalId = setInterval(findNextEvent, 60000); // Update every minute
+        findNextEvent();
+
+        return () => clearInterval(intervalId);
+    }, [groupedEvents]);
+
     const handlePress = useCallback((event: ExternalEventDetails) => {
         setSelectedEvent(event);
     }, []);
+
+    const scrollToCurrentEvent = useCallback(() => {
+        if (!scrollViewRef.current || !nextEventMarkerRef.current) {
+            console.log('Missing refs for scrolling to next event')
+            return;
+        }
+
+        nextEventMarkerRef.current?.measureLayout(
+            scrollViewRef.current.getInnerViewNode(),
+            (_, y) => {
+                scrollViewRef.current?.scrollTo({ y, animated: true });
+            }
+        );
+    }, [nextEventMarkerRef, scrollViewRef]);
+
+    useEffect(() => {
+        if (nextEvent && !didInitiallyScroll) {
+            scrollToCurrentEvent();
+            setDidInitiallyScroll(true);
+        }
+    }, [nextEvent, didInitiallyScroll, scrollToCurrentEvent]);
 
     return (
 
@@ -104,8 +168,24 @@ export const MyExternalEvents = () => {
                         setSelectedEvent(null)
                     }} />
             )}
-            <Heading size="xl" paddingHorizontal={20}>Mina bokade aktiviteter</Heading>
-            <ScrollView flex={1}>
+            <HStack justifyContent="space-between" alignItems="center" height={50} paddingLeft={20}>
+                <Heading size="xl">Mina bokade aktiviteter</Heading>
+                {nextEvent && (
+
+                        <Pressable
+                            onPress={scrollToCurrentEvent}
+                            paddingRight={15}
+                            width={50}
+                            height={50}
+                            alignItems="center"
+                            justifyContent='center'
+                        >
+                            <Icon as={CalendarClockIcon} size="xl" color="$primary200"/>
+                        </Pressable>
+
+                )}
+            </HStack>
+            <ScrollView flex={1} ref={scrollViewRef}>
                 <VStack space="lg" flex={1} justifyContent="center">
                     {loading &&
                         <LoadingScreen />
@@ -118,14 +198,18 @@ export const MyExternalEvents = () => {
                     }
                 </VStack>
                 {Object.keys(groupedEvents).map((date) => (
-
                     <VStack key={date} space="sm" paddingHorizontal={20}>
+                        {nextEvent && nextEvent.eventId === groupedEvents[date][0].eventId && (
+                            <View ref={nextEventMarkerRef} />
+                        )}
                         <Heading color="$primary900" size="lg" >{displayLocaleTimeStringDate(date ?? "")}</Heading>
                         <Divider />
                         {groupedEvents[date].map((event) => (
-                            <Pressable key={event.eventId} onPress={() => handlePress(event)}>
-
-                                <HStack key={event.eventId} space="sm" paddingVertical={10}>
+                            <Pressable key={event.eventId} onPress={() => handlePress(event)} style={{opacity: event.eventDate && nextEvent && nextEvent.eventDate && event.eventDate < nextEvent?.eventDate ? 0.5 : 1.0}}>
+                                {nextEvent && nextEvent.eventId == event.eventId && event.eventId != groupedEvents[date][0].eventId && (
+                                    <View ref={nextEventMarkerRef} />
+                                )}
+                                <HStack space="sm" paddingVertical={10}>
                                     <VStack justifyContent="flex-start" alignItems="center">
                                         <Text size="md" color='$teal500' paddingTop={2}>
                                             {event.startTime}
@@ -134,16 +218,18 @@ export const MyExternalEvents = () => {
                                             {event.endTime}
                                         </Text>
                                     </VStack>
-                                    <VStack flex={1} paddingLeft={10}>
+                                    <VStack flex={1} >
                                         <HStack space="md" justifyContent="space-between" alignItems="center">
                                             <Heading size="md" color="$primary600" style={{ flex: 1 }}>
                                                 {event.titel}
                                             </Heading>
+                                            <HStack space="sm" >
                                             {event.categories?.map((category, index) => (
-                                                <Text key={index} color="$vscode_customLiteral" style={{ paddingLeft: 10, maxWidth: 45 }}>
+                                                <Text key={index} color="$vscode_customLiteral" style={{ width: 30 }}>
                                                     {getEventCategoryBadge(category.code, `#${category.colorBackground}`)}
                                                 </Text>
                                             ))}
+                                            </HStack>
                                         </HStack>
                                         <Text color="$vscode_customLiteral" style={{ marginBottom: 10 }}>
                                             {/* <FontAwesomeIcon icon={faMountainCity} size={14} style={{ color: vscode_customLiteral, marginRight: 10 }} /> */}
