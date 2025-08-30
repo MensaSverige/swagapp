f0c56591990a09c1dfa87440cff21eb12a853d7a
Imamyable <amynylander@hotmail.com>
2025-08-30 13:40:35 +0200

    cleanup

--- mensasverige/app/login.tsx
+++ mensasverige/app/login.tsx
@@ -1,21 +1,2 @@
-// import { StyleSheet } from 'react-native';
-
-// import ParallaxScrollView from '@/components/ParallaxScrollView';
-// import { ThemedText } from '@/components/ThemedText';
-// import { ThemedView } from '@/components/ThemedView';
-
-// export default function LoginScreen() {
-//   return (
-//     <ParallaxScrollView>
-//       <ThemedView >
-//         <ThemedText type="title">Login!</ThemedText>
-//       </ThemedView>
-//     </ParallaxScrollView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-
-// });
 import { SigninForm } from "@/features/account/screens/SigninForm";
 export default SigninForm;
