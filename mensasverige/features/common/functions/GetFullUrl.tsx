aeb29036ffa0fa0d8a123993f28b1f5b5cb4ebdc
Imamyable <amynylander@hotmail.com>
2025-08-30 18:05:24 +0200

    update env variables

--- mensasverige/features/common/functions/GetFullUrl.tsx
+++ mensasverige/features/common/functions/GetFullUrl.tsx
@@ -1,10 +1,10 @@
-import { API_URL } from '@env';
-
 export const getFullUrl = (url: string): string => {
   if (url.startsWith('http://') || url.startsWith('https://')) {
     return url;
   } else {
-    const apiUrl = API_URL.endsWith('/') ? API_URL.slice(0, -1) : API_URL;
+    const apiUrl = process.env.EXPO_PUBLIC_API_URL?.endsWith('/') 
+      ? process.env.EXPO_PUBLIC_API_URL.slice(0, -1) 
+      : process.env.EXPO_PUBLIC_API_URL;
     const relativeUrl = url.startsWith('/') ? url.slice(1) : url;
     return `${apiUrl}/${relativeUrl}`;
   }
