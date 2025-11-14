31be1f2783d4afacb5056978db4970c99663169f
Imamyable <amynylander@hotmail.com>
2025-11-14 12:47:34 +0100

    remove extraneous route

--- mensasverige/app/(tabs)/(events)/_layout.tsx
+++ mensasverige/app/(tabs)/(events)/_layout.tsx
@@ -6,7 +6,6 @@ export default function TabLayout() {
   return (
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="create" options={{ headerShown: false }} />
       </Stack>
   );
 }
